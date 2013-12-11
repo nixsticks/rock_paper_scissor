931e3d213c54bf61bbf2117929a42d68c1fd453f
Nikki Thean <nikki.thean@gmail.com>
2013-12-10 19:12:32 -0500

    Update styles

--- game.rb
+++ game.rb
@@ -6,17 +6,19 @@ Bundler.require
 module Game
   class RPS_App < Sinatra::Application
 
-    game = GameEngine.new
+    before do 
+      @game ||= GameEngine.new
+    end
 
     get '/throw' do
       erb :throw
     end
 
     get '/throw/:move' do
-      @player_move = game.player.move = params[:move].to_s
-      @computer_move = game.computer.random_move
+      @player_move = @game.player.move = params[:move].to_s
+      @computer_move = @game.computer.random_move
 
-      @message = game.win_or_lose
+      @message = @game.win_or_lose
 
       erb :move
     end
